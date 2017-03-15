0076577297d16789857f8a152bae57d3af8c3ac9
Kyle Tommet <ktommet@gmail.com>
2017-03-14 23:34:53 -0700

    App Configurations

---
+++ app/src/main/java/org/erickson_foundation/miltonhericksonfoundation/AppConfig.java
@@ -0,0 +1,11 @@
+package org.erickson_foundation.miltonhericksonfoundation;
+
+/**
+ * Created by ktomm on 3/14/2017.
+ */
+
+public final class AppConfig {
+    private AppConfig(){}
+    public static final String CONFERENCE_NAME_INTENT = "ConferenceName";
+    public static final String CONFERENCE_CONTENTS_JSON = "ConferenceContents";
+}
