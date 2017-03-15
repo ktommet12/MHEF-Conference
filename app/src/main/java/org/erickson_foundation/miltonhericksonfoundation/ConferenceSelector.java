e43b66b108430a6f82c805023aae6e3c207820a4
Kyle Tommet <ktommet@gmail.com>
2017-03-14 23:37:16 -0700

    Changed it so that Conference Selector loads the conference from the server before loading into MainActivity therefore the information is readily available

--- app/src/main/java/org/erickson_foundation/miltonhericksonfoundation/ConferenceSelector.java
+++ app/src/main/java/org/erickson_foundation/miltonhericksonfoundation/ConferenceSelector.java
@@ -1,5 +1,6 @@
 package org.erickson_foundation.miltonhericksonfoundation;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
@@ -9,8 +10,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TableRow;
 
-public class ConferenceSelector extends AppCompatActivity implements View.OnClickListener {
-    TableRow couples, evolution;
+import org.erickson_foundation.miltonhericksonfoundation.DB.DBWorker;
+import org.erickson_foundation.miltonhericksonfoundation.DB.DBWorkerDelegate;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ConferenceSelector extends AppCompatActivity implements View.OnClickListener, DBWorkerDelegate {
+    private TableRow couples, evolution;
+    private DBWorker dbWorker;
+    private final String TAG = "ConfSelector";
+    private ConferenceType confType = ConferenceType.DEFAULT;
+    private ProgressDialog mProgressDialog;
+    private boolean isTaskInProgress = false;
+    private String confContents;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,19 +34,81 @@ public class ConferenceSelector extends AppCompatActivity implements View.OnClic
 
         couples.setOnClickListener(this);
         evolution.setOnClickListener(this);
+
+        dbWorker = new DBWorker();
+        dbWorker.setOnFinishedListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        startProgressDialog();
         Intent intent = new Intent(this, MainActivity.class);
         switch (v.getId()){
             case R.id.couples_conferece:
-                intent.putExtra("ConferenceName", "Couples Conference");
+                this.confType = ConferenceType.COUPLES;
                 break;
             case R.id.evolution_conferece:
-                intent.putExtra("ConferenceName", "Evolution of Psychotherapy");
+                this.confType = ConferenceType.EVOLUTION;
                 break;
         }
+        if (!isTaskInProgress && dbWorker != null){
+            dbWorker.setConferenceType(this.confType);
+            dbWorker.execute(); //initiate information download from server
+            isTaskInProgress = true;
+        }else{
+            if(dbWorker == null){
+                dbWorker = new DBWorker(this.confType);
+                dbWorker.setOnFinishedListener(this);
+                dbWorker.execute();
+            }
+        }
+
+    }
+    private void startProgressDialog(){
+        this.mProgressDialog = new ProgressDialog(this);
+        this.mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        this.mProgressDialog.setMessage("Grabbing Conference Information, Please Wait...");
+        this.mProgressDialog.setIndeterminate(false);
+        this.mProgressDialog.setCancelable(false);
+        this.mProgressDialog.show();
+    }
+    private void loadConference(){
+        Intent intent = new Intent(this, MainActivity.class);
+        switch(this.confType){
+            case EVOLUTION:
+                intent.putExtra(AppConfig.CONFERENCE_NAME_INTENT, "Evolution");
+                break;
+            case COUPLES:
+                intent.putExtra(AppConfig.CONFERENCE_NAME_INTENT, "Couples");
+                break;
+            case BRIEF:
+                intent.putExtra(AppConfig.CONFERENCE_NAME_INTENT, "Brief");
+                break;
+            default:
+                intent.putExtra(AppConfig.CONFERENCE_NAME_INTENT, "Default");
+        }
+        intent.putExtra(AppConfig.CONFERENCE_CONTENTS_JSON, confContents);
         startActivity(intent);
+    }
+    //once the app contacts the DB and its returns with either information or an error it will call didFinishTask
+    @Override
+    public void didFinishTask(JSONObject jsonObject) {
+        if(this.mProgressDialog != null && mProgressDialog.isShowing()){
+            this.mProgressDialog.hide();
+        }
+        dbWorker = null;
+        isTaskInProgress = false;
+        try{
+            //the last DBWorkerTask was a failure for some reason
+            if(jsonObject.optBoolean("wasASuccess", false) == false){
+                Log.i(TAG, "Error Contacting Server");
+            }else{
+                Log.i(TAG+" Response", jsonObject.toString());
+                this.confContents = jsonObject.toString();
+                loadConference();
+            }
+        }catch(Exception ex){
+            Log.e(TAG, ex.getMessage());
+        }
     }
 }
