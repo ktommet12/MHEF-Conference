78b10c67e2a4e30da23b87eadcc9779f0722fa60
Kyle Tommet <ktommet@gmail.com>
2017-03-14 22:27:25 -0700

    Adding Comments

--- app/src/main/java/org/erickson_foundation/miltonhericksonfoundation/DB/DBWorkerDelegate.java
+++ app/src/main/java/org/erickson_foundation/miltonhericksonfoundation/DB/DBWorkerDelegate.java
@@ -3,7 +3,9 @@ package org.erickson_foundation.miltonhericksonfoundation.DB;
 import org.json.JSONObject;
 
 /**
- * Created by User on 3/9/2017.
+ * Created by Kyle Tommet on 3/9/2017.
+ * Defines the Listener Interface that a DB Connection will use to signify that it has finished its task
+ * results are returned as JSONObject
  */
 
 public interface DBWorkerDelegate {
