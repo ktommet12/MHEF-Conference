4bde91dd5fc08d12f08e2463d98a57e0fbdd8cf6
Kyle Tommet <kyle@erickson-foundation.org>
2017-05-02 09:21:53 -0700

    in-app twitter integration

---
+++ app/src/main/java/org/erickson_foundation/miltonhericksonfoundation/Twitter/TwitterConfig.java
@@ -0,0 +1,12 @@
+package org.erickson_foundation.miltonhericksonfoundation.Twitter;
+
+/**
+ * Created by User on 5/1/2017.
+ */
+
+public final class TwitterConfig {
+    private TwitterConfig(){}
+    public static final String TWITTER_CONSUMER_KEY = "30QCzhZ13e3U7Ox29pZT7O7XB";
+    public static final String TWITTER_CONSUMER_SECRET = "hGUmN8NumH1438rkVrID8vcg3Qh0wUBziuqtjpwIsF9Z47bcT4";
+    public static final String FOUNDATION_TIMELINE = "EricksonFound";
+}
